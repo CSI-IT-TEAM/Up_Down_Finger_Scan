f75fbbbf7b62616e215df5c41991250ef2eb2733
Ngọc Nguyễn Thanh <ngoc333@gmail.com>
2025-07-01 09:50:39 +0700

    Update Door Scan

--- Up_Finger_Scan/Program.cs
+++ Up_Finger_Scan/Program.cs
@@ -15,8 +15,8 @@ namespace ATTN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           Application.Run(new UploadData());
-         //  Application.Run(new FrmDownload());
+        //   Application.Run(new UploadData());
+           Application.Run(new UploadDataDoor());
         }
     }
 }
